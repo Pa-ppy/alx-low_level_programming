67d4e31f8d70336524839d6933ed9bb168c35a26
Pa-ppy <rayjacks07@gmail.com>
2024-06-20 10:56:38 -0700

    Hello, World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
-int main(void){
+/*
+ * main - Entry point of the program
+ * 
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return 0;
+}
