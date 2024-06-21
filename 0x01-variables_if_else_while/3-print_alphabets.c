43e55f45eeed63ba8bc73bb1bb07e9913618e388
Pa-ppy <rayjacks07@gmail.com>
2024-06-21 16:55:10 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints the alphabets in lowercase and then uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char small_alpha;
+	char big_alpha;
+
+	for (small_alpha = 'a'; small_alpha <= 'z'; small_alpha++)
+		putchar(small_alpha);
+	for (big_alpha = 'A'; big_alpha <= 'Z'; big_alpha++)
+		putchar(big_alpha);
+
+	putchar('\n');
+
+	return (0);
+}
