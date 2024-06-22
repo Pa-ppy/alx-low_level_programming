6d0b2aa45f37f44f2f45b55ca38a8d94e4c6624b
Pa-ppy <rayjacks07@gmail.com>
2024-06-21 22:06:33 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,19 +15,19 @@ int main(void)
 	for (hundreds = 0; hundreds <= 9; hundreds++)
 	{
 	        for (tens = hundreds + 1; tens <= 9; tens++)
-        		{
-                		for (ones = tens + 1; ones <= 9; ones++)
-                		{
-                        		putchar('0' + hundreds);
-					putchar('0' + tens);
-                        		putchar('0' + ones);
-                        		if (!(hundreds == 7 && tens == 8 && ones != 9))
-                        		{
-                                		putchar(',');
-                                		putchar(' ');
-                        		}
-                		}
-        		}
+		{
+			for (ones = tens + 1; ones <= 9; ones++)
+			{
+				putchar('0' + hundreds);
+				putchar('0' + tens);
+				putchar('0' + ones);
+                        	if (!(hundreds == 7 && tens == 8 && ones != 9))
+				{
+                                	putchar(',');
+	                               	putchar(' ');
+        	                }
+                	}
+        	}
 	}
         putchar('\n');
 
