3e51302929b3e979aaa5b248e449d2ffb8c4c7be
Pa-ppy <rayjacks07@gmail.com>
2024-06-21 17:20:42 -0700

    C - Variables, if, else, while

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -14,5 +14,5 @@ int main(void)
 		printf("%d", number);
 	printf("\n");
 
-	return 0;
+	return (0);
 }
--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,18 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - Prints all single-digit numbers of base 10 starting from 0 with putchar
+ * main - Prints all single-digit numbers of base 10 starting from 0
  *
  * Return: Always 0 (Success)
  *
  */
 int main(void)
 {
-        int number;
+	int number;
 
-        for (number = 0; number <= 9; number++)
+	for (number = 0; number <= 9; number++)
                putchar(number + '0');
-        putchar('\n');
+	putchar('\n');
 
-        return 0;
+	return 0;
 }
