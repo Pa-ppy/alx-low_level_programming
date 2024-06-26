d5fec3b1b0c0943208d429c6076c7ede16b349f9
Pa-ppy <rayjacks07@gmail.com>
2024-06-25 18:06:56 -0700

    Functions_nested_loops

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - Entry point of the program
+ *
+ * Description: Prints "_putchar"
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char text[] = "_putchar";
+	int i;
+
+	for (i = 0; text[i] != '\0'; i++)
+		_putchar(text[i]);
+	_putchar('\n');
+	
+	return (0);
+}
