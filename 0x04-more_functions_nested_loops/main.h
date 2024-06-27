1586b6fab56f3cdee0d30553f2af5ea8826ba79a
Pa-ppy <rayjacks07@gmail.com>
2024-06-27 11:58:17 -0700

    More functions, more nested loops

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,36 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* Declaration of the _putchar function*/
+int _putchar(char a);
+
+/* Prototype of _isupper function */
+int _isupper(int c);
+
+/* Prototype of _isdigit function */
+int _isdigit(int c);
+
+/* Prototype of mul function */
+int mul(int a, int b);
+
+/* Prototype of print_numbers function */
+void print_numbers(void);
+
+/* Prototype of print_most_numbers function */
+void print_most_numbers(void);
+
+/* Prototype of more_numbers function */
+void more_numbers(void);
+
+/* Prototype of print_line function */
+void print_line(int n);
+
+/* Prototype of print_diagonal function */
+void print_diagonal(int n);
+
+/* Prototype of print_square function */
+void print_square(int size);
+
+/* Prototype of print_triangle function */
+void print_triangle(int size);
+#endif
