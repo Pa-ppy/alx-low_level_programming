a82d62f03ad013d1d81ceabbfd2176c5f442462d
Pa-ppy <rayjacks07@gmail.com>
2024-07-04 18:27:43 -0700

    Task 7

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -12,12 +12,15 @@ char *leet(char *str)
 	char letters[] = "aAeEoOtTlL";
 	char leet[] = "4433007711";
 
-	for (i = ; str[i] != '\0'; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		for (j = 0; letters[j] != '\0'; j++)
 		{
 			if (str[i] == letters[j])
+			{
 				str[i] = leet[j];
+				break;
+			}
 		}
 	}
 
