ab73d4b2ec02bdbe4e720aae435c262b58361d8e
Pa-ppy <rayjacks07@gmail.com>
2024-07-04 19:39:29 -0700

    Added Header file

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -24,6 +24,8 @@ char *rot13(char *);
 
 void print_number(int n);
 
+int str_len(char *str);
+void reverse_string(char *str);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 
 void print_buffer(char *b, int size);
