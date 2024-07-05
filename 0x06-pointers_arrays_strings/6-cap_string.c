607fc698c65e85a8e04d36eb2314431b93f3c05a
Pa-ppy <rayjacks07@gmail.com>
2024-07-04 18:01:38 -0700

    Task 6

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -14,6 +14,9 @@ char *cap_string(char *str)
 
 	while (str[i] != '\0')
 	{
+		if (capitalize_char && str[i] >= 'a' && str[i] <= 'z')
+			str[i] -= ('a' - 'A');
+		capitalize_char = 0;
 		for (int j = 0; separators[j] != '\0'; j++)
 		{
 			if (str[i] == separators[j])
@@ -22,15 +25,6 @@ char *cap_string(char *str)
 				break;
 			}
 		}
-		if (capitalize_char && str[i] >= 'a' && str[i] <= 'z')
-		{
-			str[i] = str[i] - ('a' - 'A');
-			capitalize_char = 0;
-		}
-		else if (str[i] >= 'A' && str[i] <= 'Z')
-			capitalize_char = 0;
-		else if (str[i] >= '0' && str[i] <= '9')
-			capitalize_next = 0;
 		i++;
 	}
 
