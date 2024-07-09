7d8a91dd22364f1021698ae96a3f08cafb4680c6
Pa-ppy <rayjacks07@gmail.com>
2024-07-08 20:10:49 -0700

    Task 7

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,7 +13,7 @@ void print_diagsums(int *a, int size)
 
 	for (i = 0; i < size; i++)
 	{
-		primary_sum == a[i * size + i];
+		primary_sum += a[i * size + i];
 		secondary_sum += a[i * size + (size - 1 - i)];
 	}
 	printf("%d, %d\n", primary_sum, secondary_sum);
