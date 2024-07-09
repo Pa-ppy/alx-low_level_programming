5510506234d85cd8fada68980acdabff52da6873
Pa-ppy <rayjacks07@gmail.com>
2024-07-08 20:07:47 -0700

    Task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * print_diagsums - Prints the sum of two diagonals
+ * @a: Pointer to the square matrix
+ * @size: Size of the square matrix
+ */
+void print_diagsums(int *a, int size)
+{
+	int primary_sum = 0;
+	int secondary_sum = 0;
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		primary_sum == a[i * size + i];
+		secondary_sum += a[i * size + (size - 1 - i)];
+	}
+	printf("%d, %d\n", primary_sum, secondary_sum);
+}
