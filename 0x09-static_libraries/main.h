d408bf57b2f6dfb07a5a7dccd8acebd10095a6bd
Pa-ppy <rayjacks07@gmail.com>
2024-07-12 15:20:09 -0700

    Added Header file

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -32,7 +32,7 @@ int _strcmp(char *s1, char *s2);
 
 char *_memset(char *s, char b, unsigned int n);
 
-char *memcpy(char *dest, char *src, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
 
 char *_strchr(char *s, char c);
 
