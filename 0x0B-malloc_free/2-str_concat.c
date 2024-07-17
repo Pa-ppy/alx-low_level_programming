79e5c8c712de76a88b862ee64a8d177e2e38c479
Pa-ppy <rayjacks07@gmail.com>
2024-07-16 19:39:01 -0700

    Task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -19,7 +19,7 @@ char *str_concat(char *s1, char *s2)
 		s2 = "";
 	for (len1 = 0; s1[len1] != '\0'; len1++)
 		;
-	for (len2 = 0; s2[len2] != '\0s len2++)
+	for (len2 = 0; s2[len2] != '\0'; len2++)
 		;
 	concat_str = malloc((len1 + len2 + 1) * sizeof(char));
 
