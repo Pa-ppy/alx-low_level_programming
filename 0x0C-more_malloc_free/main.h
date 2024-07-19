e7ef919ed539b67ac03b3e7399e64e08b4f5f927
Pa-ppy <rayjacks07@gmail.com>
2024-07-18 18:00:57 -0700

    Added Header file

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,7 +4,7 @@
 /** Function prototypes **/
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
-char *string_noncat(char *s1, char *s2, unsigned int n);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
