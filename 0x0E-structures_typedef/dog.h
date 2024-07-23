dcd739ae277b054ef644bf2c962a862e1b38b55b
Pa-ppy <rayjacks07@gmail.com>
2024-07-22 20:05:16 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - A structure representing a dog
+ * @name: Pointer to the dog's name
+ * @age: Dog's age
+ * @owner: Pointer to the dog's owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
