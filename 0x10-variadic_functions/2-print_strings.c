66f50fc86503537edbbf29a9c5a265e780041340
Pa-ppy <rayjacks07@gmail.com>
2024-07-25 06:24:13 -0700

    Task 2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 		if (str == NULL)
 			printf("(nil)");
 		else
-			printf("%s", separator);
+			printf("%s", str);
 
 		if ((separator != NULL) && (i < n - 1))
 			printf("%s", separator);
