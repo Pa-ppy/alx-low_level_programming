bf92951de6da1a6256113c8c9ddd3e399903676c
Pa-ppy <rayjacks07@gmail.com>
2024-07-25 17:04:25 -0700

    Task 1

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,3 +1,4 @@
+#include <stddef.h>
 #include "function_pointers.h"
 
 /**
@@ -12,9 +13,9 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	size_t i;
 
-	if (array != NULL && action != NULL)
-	{
-		for (i = 0; i < size; i++)
-			action(array[i]);
-	}
+	if ((array == NULL) || (action == NULL))
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
 }
