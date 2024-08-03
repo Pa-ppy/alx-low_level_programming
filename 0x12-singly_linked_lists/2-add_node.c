d05894f3e109aabd8a0735f754ddb66840eef7f2
Pa-ppy <rayjacks07@gmail.com>
2024-08-02 18:56:35 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,21 +3,6 @@
 #include <string.h>
 
 /**
- * _strlen: - Returns the length of a string
- * @s: String to check
- *
- * Return: The length of the string
- */
-static unsigned int _strlen(const char *s)
-{
-	unsigned int len = 0;
-
-	while (s[len])
-		len++;
-	return (len);
-}
-
-/**
  * add_node - Adds a new node at the start of list_t
  * @head: Double pointer to the head of list_t
  * @str: New string to be added
@@ -27,22 +12,24 @@ static unsigned int _strlen(const char *s)
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	unsigned int len = _strlen(str);
+	unsigned int len;
 
-	new_node = malloc(sizeof(list_t));
-	if (!new_node)
+	if (str == NULL)
 		return (NULL);
 
-	new_node->str = malloc(len + 1);
-	if (!new_node->str)
+	new_node = malloc(sizeof(list_t));
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	for (unsigned int i = 0; i < len; i++)
-		new_node->str[i] = str[i];
-	new_node->str[len] = '\0';
+	for (len = 0; str[len]; len++)
+		;
 
 	new_node->len = len;
 	new_node->next = *head;
