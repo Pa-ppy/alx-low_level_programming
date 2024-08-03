e031c113f4f4066060a5fc4d61e817991edb3e49
Pa-ppy <rayjacks07@gmail.com>
2024-08-02 18:18:16 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,21 @@
 #include <string.h>
 
 /**
+ * _strlen: - Returns the length of a string
+ * @s: String to check
+ *
+ * Return: The length of the string
+ */
+static unsigned int _strlen(const char *s)
+{
+	unsigned int len = 0;
+
+	while (s[len])
+		len++;
+	return (len);
+}
+
+/**
  * add_node - Adds a new node at the start of list_t
  * @head: Double pointer to the head of list_t
  * @str: New string to be added
@@ -12,24 +27,23 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
-	char *new_str;
-	unsigned int len = 0;
+	unsigned int len = _strlen(str);
 
 	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (!new_node)
 		return (NULL);
 
-	new_str = strdup(str);
-	if (new_str == NULL)
+	new_node->str = malloc(len + 1);
+	if (!new_node->str)
 	{
 		free(new_node);
 		return (NULL);
 	}
 
-	while (str[len])
-		len++;
+	for (unsigned int i = 0; i < len; i++)
+		new_node->str[i] = str[i];
+	new_node->str[len] = '\0';
 
-	new_node->str = new_str;
 	new_node->len = len;
 	new_node->next = *head;
 	*head = new_node;
