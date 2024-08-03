5ecf532674257097e02fe11a6a45fa67a050fb7a
Pa-ppy <rayjacks07@gmail.com>
2024-08-02 17:24:42 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,22 +3,22 @@
 
 /**
  * print_list - Prints all elements of a list
- * @l: Pointer to the list_t list
+ * @h: Pointer to the list_t list
  *
  * Return: The number of nodes in the list_t
  */
-size_t print_list(const list_t *l)
+size_t print_list(const list_t *h)
 {
 	size_t count_node = 0;
 
-	while (l)
+	while (h)
 	{
-		if (l->str == NULL)
+		if (h->str == NULL)
 			printf("[0] (nil)\n");
 		else
-			printf("[%u] %s\n", l->len, l->str)
+			printf("[%u] %s\n", h->len, h->str)
 
-		l = l->next;
+		h = h->next;
 		count_node++;
 	}
 
