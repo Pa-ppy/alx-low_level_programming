c485024f3825fffb31199ddb021c7f2c8d1b85e3
Pa-ppy <rayjacks07@gmail.com>
2024-08-06 10:00:44 -0700

    Task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_listint - Prints all the elements of a listing
+ * @h: Pointer to the listint_t list
+ *
+ * Return: The number of nodes
+ */
+size_t print_listint(const listint_ *h)
+{
+	size_t count_variable = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count_variable++;
+	}
+
+	return (count_variable);
+}
