0b6c6e150ab3d8c40d4a9ef513394279e639a654
Pa-ppy <rayjacks07@gmail.com>
2024-08-14 10:50:48 -0700

    Task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,8 +2,8 @@
 
 #define BUFFER_SIZE 1024
 
-void close_file(int var_file, const char *filename);
-void print_error(const char *msg, const char *filename, int exit_code);
+void close_file(int var_file);
+void error_exit(const char *msg, int exit_code);
 
 /**
  * main - Entry point for the cp program
@@ -19,42 +19,28 @@ int main(int argc, char *argv[])
 	char buffer[BUFFER_SIZE];
 
 	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
+		error_exit("Usage: cp file_from file_to\n", 97);
 
 	file_from = open(argv[1], O_RDONLY);
 	if (file_from == -1)
-		print_error("Error: Can't read from file", argv[1], 98);
+		error_exit("Error: Can't read from file", 98);
 
 	file_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (file_to == -1)
-	{
-		close_file(file_from, argv[1]);
-		print_error("Error: Can't write to", argv[2], 99);
-	}
+		error_exit("Error: Can't write to file ", 99);
 
 	while ((no_read = read(file_from, buffer, BUFFER_SIZE)) > 0)
 	{
 		no_written = write(file_to, buffer, no_read);
 		if (no_written == -1 || no_written != no_read)
-		{
-			close_file(file_from, argv[1]);
-			close_file(file_to, argv[2]);
-			print_error("Error: Can't write to", argv[2], 99);
-		}
+			error_exit("Error: Can't write to file ", 99);
 	}
 
 	if (no_read == -1)
-	{
-		close_file(file_from, argv[1]);
-		close_file(file_to, argv[2]);
-		print_error("Error: Can't read from file", argv[1], 98);
-	}
+		error_exit("Error: Can't read from file ", 98);
 
-	close_file(file_from, argv[1]);
-	close_file(file_to, argv[2]);
+	close_file(file_from);
+	close_file(file_to);
 
 	return (0);
 }
@@ -62,9 +48,8 @@ int main(int argc, char *argv[])
 /**
  * close_file - Closes a file descriptor amd handles error
  * @var_file: File descriptor to close
- * @filename: Name of file associated
  */
-void close_file(int var_file, const char *filename)
+void close_file(int var_file)
 {
 	if (close(var_file) == -1)
 	{
@@ -74,13 +59,13 @@ void close_file(int var_file, const char *filename)
 }
 
 /**
- * print_error - Prints an error message
+ * error_exit - Prints an error message
  * @msg: Error message to print
  * @filename: Filename associated
  * @exit_code: The exit code
  */
-void print_error(const char *msg, const char *filename, int exit_code)
+void error_exit(const char *msg, int exit_code)
 {
-	dprintf(STDERR_FILENO, "%s %s\n", msg, filename);
+	dprintf(STDERR_FILENO, "%s", msg);
 	exit(exit_code);
 }
