c1381808b56421dca810702ec10f91d6ef00812e
Pa-ppy <rayjacks07@gmail.com>
2024-09-19 19:51:33 -0700

    Task 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,11 +2,11 @@
 #include <stdlib.h>
 
 /**
- * delete_dnodeint_at_index - Deletes the node at a given index
+ * delete_dnodeint_at_index - Deletes the node at given index
  * @head: Double pointer to the head of the list
  * @index: index of the node to be deleted, starting from 0
  *
- * Return: 1 if successful, -1 if failed
+ * Return: 1 if successful, -1 if it failed
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
@@ -15,15 +15,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 
 	if (!*head)
 		return (-1);
-
-	if (index = 0)
-	{
-		*head = temp->next;
-		if (temp->next)
-			temp->next->prev = NULL;
-		free(temp);
-		return (1);
-	}
 
 	while (temp && i < index)
 	{
@@ -34,10 +25,19 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	if (!temp)
 		return (-1);
 
-	if (temp->next)
-		temp->next->prev = temp->prev;
-	if (temp->prev)
-		temp->prev->next = temp->next;
+	if (temp == *head)
+	{
+		*head = temp->next;
+		if (temp->next)
+			temp->next->prev = NULL;
+	}
+	else
+	{
+		if (temp->prev)
+			temp->prev->next = temp->next;
+		if (temp->next)
+			temp->next->prev = temp->prev;
+	}
 
 	free(temp);
 	return (1);
