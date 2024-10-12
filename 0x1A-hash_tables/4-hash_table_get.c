375f826af4e20831d3ddf0cd56f40bb1041cb022
Pa-ppy <rayjacks07@gmail.com>
2024-10-11 17:52:51 -0700

    Task 4

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -18,7 +18,7 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	index = key_index((const unsigned char *)key, ht->size);
 	node = ht->array[index];
 
-	while (node)
+	while (node != NULL)
 	{
 		if (strcmp(node->key, key) == 0)
 			return (node->value);
